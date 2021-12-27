f51859a78c609a22cce0a42ad0af12fb7430740f
Alberto Frison <alberto.frison@gmail.com>
2021-12-27 18:38:21 +0100

    Final commit

--- Script.R
+++ Script.R
@@ -1,9 +1,9 @@
 # MAVEN ANALYTICS HARRY POTTER SCRIPT
-
 library(ggplot2)
 library(tidyverse)
 library(ggthemes)
 library(tidytext)
+library(purrr)
 
 setwd("~/R/Harry_Potter_Movies")
 characters <- read.csv("Characters.csv")
@@ -23,6 +23,8 @@ characters$Wand..Core. <- ifelse (characters$Wand..Core.=="","Unknown",character
 characters[which(characters$House == "Beauxbatons Academy of Magic"),]$House <- "Other"
 characters[which(characters$House == "Durmstrang Institute"),]$House <- "Other"
 
+characters[which(characters$Patronus == "Jack rabbit"),]$Patronus <- "Rabbit"
+
 
 characters <- data.frame(characters)
 
@@ -30,29 +32,31 @@ characters %>%
   filter (House != "Unknown") %>%
   ggplot () +
   geom_bar (aes(x=fct_infreq(House), fill = Gender)) + 
-  labs(x= "House", y = "Totals", title = "Characters split by House and Gender", subtitle = "Now you get all why the rush to get a girl for the prom" ) +
+  labs(x= "", y = "", title = "Characters split by House and Gender", subtitle = "Now you get why all the rush to get a girl for the prom" ) +
   scale_y_continuous(breaks = seq(0, 100, by = 2)) +
-  theme_few() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom")
+  theme_light(base_size = 15)
+  #theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom")
 
 characters %>%
   filter (Wand..Core. != "Unknown") %>%
   ggplot () +
   geom_bar (aes(x= fct_infreq(Wand..Core.), fill = Character.Name)) +   #beware of the fct_infreq tidyverse function!
-  labs(x= "Wand Core", y = "Totals", title = "Wands split by Core and Wood", subtitle = "Harry and Tom Riddle (aka Voldemort) have both a Phoenix Feather Wand" ) +
+  labs(x= "", y = "", title = "Wands Type by Owner", subtitle = "Harry and Tom Riddle (aka Voldemort) use a Phoenix Feather Wand" ) +
   scale_y_continuous(breaks = seq(0, 10, by = 1)) +
-  theme_few() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom")
+  theme_light(base_size = 15) +
+  theme(legend.title = element_blank())
+
 
 characters %>%
   filter (Patronus != "Unknown") %>%
   ggplot () +
   geom_bar (aes(x= fct_infreq(Patronus), fill = Species)) +   #beware of the fct_infreq tidyverse function!
-  labs(x= "Patronus", y = "Totals", title = "Patronus Form split by Species", subtitle = "Nothing to add..." ) +
+  labs(x= "", y = "", title = "Patronus Form split by Species", subtitle = "Nothing to add here..." ) +
   scale_y_continuous(breaks = seq(0, 10, by = 1)) +
-  theme_few() +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom")
-###############################  END OF CHARACTERS ############################################################################################
+  theme_light(base_size = 15) +
+  theme(legend.title = element_blank()) +
+  theme(axis.text.x = element_text(size =11))
+  ###############################  END OF CHARACTERS ############################################################################################
 
 chapters <- read.csv("Chapters.csv")
 chapters <- data.frame(chapters)
@@ -70,7 +74,8 @@ chapters %>%
 dialogues <- read.csv("Dialogue.csv")
 places <- read.csv("Places.csv")
 movies <- read.csv("Movies.csv")
-movies$Movie.ID <- movies$?..Movie.ID
+movies$Movie <- movies$ï..Movie.ID
+movies$Movie.ID <- movies$Movie
 
 
 dialogues_1 <- merge(dialogues, characters[ , c("Character.ID", "Character.Name", "Gender", "House")], by = "Character.ID") #OK
@@ -82,40 +87,11 @@ good_guys <- c("Harry Potter", "Lily Potter", "Ron Weasley","Hermione Granger", 
 bad_guys <- c("Severus Snape", "Voldemort", "Draco Malfoy", "Dolores Umbridge", "Cornelius Fudge","Vernon Dursley", "Bellatrix Lestrange", "Lucius Malfoy", "Tom Riddle", "Gilderoy Lockhart")
 
 dialogues_4$Character.Type <- ifelse (dialogues_4$Character.Name %in% good_guys, "Good", ifelse(dialogues_4$Character.Name %in% bad_guys, "Bad", "Unknown"))
-
 dialogues_4$Dialogue.Lenght <- str_count(dialogues_4$Dialogue, '\\w+')
-
-dialogues_4 %>%
-  filter ((Gender != "Unknown") & (Dialogue.Lenght <75)) %>%
-  ggplot() +
-  geom_density(aes(x=Dialogue.Lenght, fill = Gender), alpha = 0.5)
-
-dialogues_4 %>%
-  # filter (Movie.ID == 1) %>%
-  arrange(Movie.ID) %>%
-  ggplot () +
-  geom_bar(aes(x= Movie.Title, fill = Character.Name)) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "")
-#facet_grid(rows = ~Movie.ID)
-
-
-#load of sentiment libraries
 
 word_library_affin <- get_sentiments("afinn") # numeric from -5 to +5
 word_library_bing <- get_sentiments("bing") # negative vs positive
 word_library_nrc <- get_sentiments("nrc") # feelings (trust, fear)
-
-# OLD CODE#################################################################################
-sentence_valuator <- function (sentence) {
-  words <- strsplit(sentence, " ")
-  val <- vector()
-  for (i in unlist(words)) {
-    val <- c(val,as.integer(word_library[which(word_library$word== str_replace_all(i,"[^[:alnum:]]", "")),]$value))
-    #print (as.integer(word_library[which(word_library$word== str_replace_all(i,"[^[:alnum:]]", "")),]$value))
-    #print(val)
-  }
-  return (prod(val))
-}
 
 
 ###### FUNCTION TO SPLIT A SENTENCE INTO SINGLE WORDS#
@@ -128,16 +104,7 @@ sentence_split <- function (sentence) {
   val
 }
 
-dialogues_4$Dialogue.Value <- sapply(dialogues_4$Dialogue,sentence_valuator)
-dialogues_4$Dialogue.Class <- as.factor(case_when(
-  dialogues_4$Dialogue.Value == 0 ~ "01. NEUTRAL",
-  dialogues_4$Dialogue.Value > 0  ~ "02. POSITIVE",
-  dialogues_4$Dialogue.Value < 0 ~ "03. NEGATIVE",
-  TRUE  ~ "04. OTHER"
-))
 ##########################################################################################
-
-
 
 ###### FUNCTION TO CREATE DATAFRAME OF EACH SINGLE WORD WITH "VALUE"
 dialogues_5 <- data.frame (
@@ -180,9 +147,40 @@ for (i in c(1:nrow(dialogues_4))) {
   }
 }
 
+############################## SPELLS ############################################
+spells <-read.csv("Spells.csv")
+dialogues_5$word <- tolower(dialogues_5$word)
+list_of_spells <- tolower(str_replace_all(map(strsplit(spells$Incantation, " "),1),"[^[:alnum:]]", ""))
 
-#as.factor(fct_infreq(place)
-# orders the characters by number of words said
+dialogues_5$is_spell <- ifelse(tolower(dialogues_5$word) %in% list_of_spells,TRUE,FALSE)
+
+# all spells
+dialogues_5 %>%
+  filter(is_spell == TRUE) %>%
+  ggplot(aes(x = fct_infreq(tolower(house)), fill = fct_infreq(character))) +
+  geom_bar() +
+  labs(x="", y = "", title = "Spells by Character and House", subtitle = "Sorry Hufflepuff, but your spell spot was taken by the Ghost in the Movies" ) +
+  theme_light(base_size = 15) + 
+  theme(legend.title = element_blank()) +
+  guides(fill=guide_legend(ncol=1))
+  
+
+  
+
+# avada kavadra and others
+dialogues_5 %>%
+  filter(is_spell == TRUE & word %in% c("avada", "crucio", "imperio")) %>%
+  ggplot(aes(x = fct_infreq(tolower(house)), fill = fct_infreq(movie))) +
+  geom_bar() +
+  #theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom")+
+  labs(x= "", y = "", title = "Unforgivable Courses", subtitle = "" ) +
+  scale_y_discrete(breaks = seq(0,10,by=2))+
+  labs(x="", y = "", title = "Even casting 3x more Unforgivable Courses than your enemy will not grant you victory", subtitle = "Maybe Severus Snape's should count as Gryffindor, but nah..." )+
+  theme_light(base_size = 15) + 
+  theme(legend.title = element_blank()) +
+  guides(fill=guide_legend(ncol=1))
+  
+
 
 dialogues_5 %>%
   filter (value_bing != "") %>%
@@ -195,7 +193,11 @@ dialogues_5 %>%
   filter (value_nrc!="") %>%
   ggplot (aes(x= fct_infreq(value_nrc))) +
   geom_bar()+
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom")
+  #theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom")
+  labs(x="", y = "", title = "Sentiment Analyisis of Dialogues in the Movies", subtitle = "" )+
+  theme_light(base_size = 15) + 
+  theme(legend.title = element_blank()) +
+  guides(fill=guide_legend(ncol=1))
 
 dialogues_5 %>%
   filter (value_nrc!="") %>%
@@ -210,11 +212,6 @@ dialogues_5 %>%
   geom_bar()+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom")
 
-#geom_boxplot () +
-#facet_grid(movie ~.)
-
-
-
 # SOME SELECTION OF PARAMETERS JUST TO TEST IN GGPLOT
 feelings <- c("anger", "positive", "anticipation", "negative", "fear", "joy")  
 place <- c("Great Hall","Headmaster's Office", "Defense Against the Dark Arts Classroom", "	The Burrow", "e	4 Privet Drive", "Griffindor Common Room", "Hagrid's Hut", "Potions Classroom", "	12 Grimmauld Place", "Ministry of Magic", "Pensieve")
@@ -227,60 +224,12 @@ dialogues_5 %>%
   geom_bar(position = "fill")+
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom") 
 
-
 dialogues_5 %>%
   filter (value_bing != "" & value_nrc %in% feelings & character %in% c("Harry Potter","Rubeus Hagrid", "Severus Snape","Bellatrix Lestrange", "Draco Malfoy", "Voldemort", "Tom Riddle", "Albus Dumbledore", "Hermione Granger", "Ron Weasley")   ) %>%
   ggplot (aes(y = fct_infreq(character), fill = value_nrc)) +
   geom_bar(position = "fill")+
-  facet_grid( value_bing ~.)
-
-#geom_text(aes(label = value_nrc), position=position_stack(vjust=0.5))
-#theme (axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom") 
-
-
-
-# DIALOGUES 4 IS OBSOLETE
-###################################################################################################################
-dialogues_4 %>%
-  filter (Dialogue.Value !=0) %>%
-  ggplot (aes(x= log10(abs(Dialogue.Value)), y = Movie.ID, color = Dialogue.Class, label = Dialogue)) +
-  geom_point(size =2) +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom") + 
-  # geom_label() +
-  scale_y_discrete (labels = titles)
-
-
-dialogues_4 %>%
-  filter (Character.Name %in% c("Harry Potter","Rubeus Hagrid", "Severus Snape","Bellatrix Lestrange", "Draco Malfoy", "Voldemort", "Tom Riddle", "Albus Dumbledore", "Hermione Granger", "Ron Weasley")) %>%
-  ggplot () +
-  geom_bar(aes(y= fct_infreq(Character.Name), fill = as.factor(Dialogue.Class)), position = "fill") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom")
-
-dialogues_4 %>%
-  filter (House %in% c("Gryffindor", "Slytherin", "Hufflepuff","Ravenclaw")) %>%
-  ggplot () +
-  geom_bar(aes(y= fct_infreq(House), fill = as.factor(Dialogue.Class)), position = "fill") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom") +
-  facet_wrap(Movie.ID ~.)
-
-dialogues_4 %>%
-  filter (Character.Name %in% c("Harry Potter", "Draco Malfoy")) %>%
-  ggplot () +
-  geom_bar(aes(y= Character.Name, fill = as.factor(Dialogue.Class)), position = "fill") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom")
-#facet_grid(Movie.ID ~.)
-
-dialogues_4 %>%
-  filter (Character.Name %in% c("Sirius Black", "Bellatrix Lestrange")) %>%
-  ggplot () +
-  geom_bar(aes(y= Character.Name, fill = as.factor(Dialogue.Class)), position = "fill") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "bottom")
-#facet_grid(Movie.ID ~.)
-
-
-dialogues_4 %>%
-  filter (Character.Type != "Unknown") %>%
-  ggplot () +
-  geom_bar(aes(y= Character.Name, fill = as.factor(Dialogue.Class)), position = "fill") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))+
-  facet_grid(Character.Type ~.)
+  facet_grid( value_bing ~.) +
+  labs(x="", y = "", title = "Feelings of selected Characters deducted by all Dialogues", subtitle = "Classification is done on a per word/dialogue basis", caption ="For a brief how to and packages: https://www.tidytextmining.com/sentiment.html")+
+  theme_light(base_size = 15) + 
+  theme(legend.title = element_blank()) +
+  guides(fill=guide_legend(ncol=1))
